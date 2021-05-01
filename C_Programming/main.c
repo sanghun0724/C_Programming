76f22da38a336ab5dc81cbff7931fdb766405159
sanghun0724 <sanghun0724@naver.com>
2021-05-01 18:23:27 +0900

    Initial Commit

---
+++ C_Programming/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_Programming
+//
+//  Created by sangheon on 2021/05/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
